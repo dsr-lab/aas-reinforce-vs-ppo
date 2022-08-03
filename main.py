1a54ea20cd904ce857c5b1f18b6185d4a8b5a627
Daniele Sirocchi <daniele.sirocchi@xonne.it>
2022-08-03 06:57:51 +0200

    Added KL check for early stopping. Some parameters update. Code refactoring. Added seeds.

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import config
 from environment.env_wrapper import EnvironmentWrapper, RenderMode
 from model_trainer import ModelTrainer
 
+import random
+import numpy as np
+import tensorflow as tf
+
+
+def set_seeds():
+    random.seed(3105)
+    np.random.seed(3105)
+    tf.random.set_seed(3105)
+
 
 def main():
+    set_seeds()
     env = EnvironmentWrapper(num=config.N_AGENTS, env_name=config.GAME_NAME, render_mode=RenderMode.off)
     model_trainer = ModelTrainer(environment=env)
     model_trainer.train()
